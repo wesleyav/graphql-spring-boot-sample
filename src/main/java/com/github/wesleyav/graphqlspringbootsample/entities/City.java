8559fd6d3493b41870574bad19d62e9cae60255e
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2022-02-17 18:09:14 -0300

    Adiciona anotation @Temporal na propriedade Date da Classe City

--- src/main/java/com/github/wesleyav/graphqlspringbootsample/entities/City.java
+++ src/main/java/com/github/wesleyav/graphqlspringbootsample/entities/City.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -34,6 +36,7 @@ public class City {
 	private Country country_id;
 
 	@Column(name = "last_update")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date last_update;
 
 }
